a815e10da3a528a3564aa89d9dc3ff6f4b8aaec8
Depak Borhara <depakborhara@gmail.com>
2023-12-11 14:09:43 -0500

    Addeed case insensitivity search, tests, and proper error handling

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
-    println!("Looking for {}", &config.query);
-    println!("In file: {}", &config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error:{e}");
+        eprintln!("Application error:{e}");
         process::exit(1);
     }
 }
